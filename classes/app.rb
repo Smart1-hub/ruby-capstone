bf0cf82cbfab9f861ed3431ef7397bf7a5505b64
Ademola Adebayo <scdk@yahoo.com>
2022-08-17 13:33:10 +0100

    Update app and add unit test for book and label

--- classes/app.rb
+++ classes/app.rb
@@ -1,12 +1,62 @@
+require_relative 'classes/book'
+require_relative 'classes/label'
+require_relative 'data/data_book'
 require_relative '../data/author_data'
 require_relative '../data/game_data'
+require 'date'
 
 class App
   def initialize
+    @all_books = []
+    @all_labels = []
     @author_data = AuthorData.new
     @game_data = GameData.new
     @authors = @author_data.load_authors
     @games = @game_data.load_games
+  end
+
+  def run
+    load_preserve_data
+    puts 'Welcome To The Catalog of My Things!'
+    puts
+    puts
+    until list_options
+      input = gets.chomp
+      if input == '10'
+        puts 'Have good day :)'
+        break
+      end
+      option(input)
+    end
+  end
+
+  def list_books
+    puts
+    puts 'No books to show! Please add a book.' if @all_books.empty?
+
+    @all_books.each do |book|
+      puts '----------------'
+      puts "ID: #{book.id}"
+      puts "Publisher: #{book.publisher}"
+      puts "Cover State: #{book.cover_state}"
+      puts "Label: \'#{book.label.title}\', \'#{book.label.color}\'"
+      puts "Publish Date: #{book.publish_date}"
+      puts '----------------'
+      puts
+    end
+    puts
+    puts
+    sleep 0.75
+  end
+
+  def list_labels
+    puts
+    puts 'No labels to show! Please add a label.' if @all_labels.empty?
+
+    @all_labels.each { |label| puts "Id: \'#{label.id}\', Title: \'#{label.title}\', Color: \'#{label.color}\'" }
+    puts
+    puts
+    sleep 0.75
   end
 
   def add_author(author)
@@ -36,8 +86,48 @@ class App
     end
   end
 
+  def create_book
+    puts
+    print 'Publisher: '
+    book_publisher = gets.chomp
+
+    print 'Cover State: '
+    book_cover_state = gets.chomp
+
+    print 'Label Title: '
+    book_label_title = gets.chomp
+
+    print 'Label Color: '
+    book_label_color = gets.chomp
+
+    print 'Publish Date (DD-MM-YYYY): '
+    book_publish_date = gets.chomp
+
+    puts
+
+    label = Label.new book_label_title, book_label_color
+
+    mybook = Book.new book_publisher, book_cover_state, book_publish_date
+
+    @all_books << mybook
+    @all_labels << label
+
+    label.add_item(mybook)
+
+    save_book(book_publisher, book_cover_state, book_label_title, book_label_color, book_publish_date)
+
+    puts 'Book created successfully.✅'
+    puts
+    puts
+    sleep 0.75
+  end
+
   def save_files
     @author_data.save_authors(@authors)
     @game_data.save_games(@games)
+  end
+
+  def load_preserve_data
+    load_books_and_labels @all_books, @all_labels
   end
 end
