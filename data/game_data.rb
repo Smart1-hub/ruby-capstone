5e395df48768905815bb8ea6ff367ae4292bb965
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-08-17 09:40:02 +0000

    Save game data to file

--- data/game_data.rb
+++ data/game_data.rb
@@ -1,52 +1,31 @@
 require 'json'
-require_relative '../classes/author'
+require 'fileutils'
+require_relative '../classes/game'
 
-def load_games_and_authors(game_list, author_list)
-  if File.exist?('./data/games.json')
-    file = File.open('.data/games.json')
+class GameData
+  def initialize
+    @games_path = 'data/games.json'
+    File.write(@games_path, []) unless File.exist?(@games_path)
+  end
 
-    if file.size.zero?
-      game_list << []
-      author_list << []
-    else
-      games = JSON.parse(File.read('./data/games.json'))
-
-      games.each do |game|
-        author = Author.new(game['first_name'], game['last_name'])
-        game = Game.new(game['publish_date'], game['multiplayer'], game['last_played_at'])
-
-        game_list << game
-        author_list << author
-        author.add_item(game)
-      end
+  def load_games
+    loaded_games = []
+    games = JSON.parse(File.read(@games_path))
+    games.each do |game|
+      loaded_games << Game.new(game['publish_date'], game['multiplayer'], game['last_played_at'])
     end
-    file.close
-  else
-    game_list << []
-    author_list << []
-  end
-end
-
-def save_game(first_name, last_name, publish_date, multiplayer, last_played_at)
-  obj = {
-    first_name: first_name,
-    last_name: last_name,
-    publish_date: publish_date,
-    multiplayer: multiplayer,
-    last_played_at: last_played_at
-  }
-
-  return unless File.exist?('./data/games.json')
-
-  if file.size.zero?
-    game = [obj]
-  else
-    game = JSON.parse(File.read('./data/games.json'))
-    game << obj
+    loaded_games
   end
 
-  file.close
-  myfile = File.open('./data/games.json', 'w')
-  myfile.write(JSON.pretty_generate(game))
-  myfile.close
+  def save_games(games)
+    saved_games = []
+    games.each do |game|
+      saved_games << {
+        publish_date: game.publish_date,
+        multiplayer: game.multiplayer,
+        last_played_at: game.last_played_at
+      }
+    end
+    File.write(@games_path, JSON.generate(saved_games))
+  end
 end
