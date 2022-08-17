42decddace68ac3bcfe3a41c542975ae3de2340e
Felix Odete <felix.odette@gmail.com>
2022-08-17 12:20:11 +0300

    RSpec linter fix

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -3,7 +3,7 @@ require_relative '../genre/controller'
 require_relative '../music_album/music_album'
 
 describe Genre do
-  include GenresController
+  include GenreController
   it 'Item should be added to array' do
     genre = Genre.new('genre')
     music_album = MusicAlbum.new('test', '20/10/2010', 'test')
