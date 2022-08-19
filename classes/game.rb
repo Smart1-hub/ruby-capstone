43277f2b60280671ae48fc44d75ac7d21b18290e
Felix Odete <felix.odette@gmail.com>
2022-08-19 17:18:37 +0300

    Add json dependency and add attr_accessor

--- classes/game.rb
+++ classes/game.rb
@@ -1,16 +1,17 @@
+require 'json'
 require_relative '../item'
 require 'date'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at
+  attr_accessor :multiplayer, :last_played_at, :publish_date
 
-  def initialize(publish_date, multiplayer, last_played_at)
-    super(publish_date)
+  def initialize(multiplayer, last_played_at, publish_date, id = nil, archived: false)
+    super(publish_date, id, archived: archived)
     @multiplayer = multiplayer
-    @last_played_at = last_played_at
+    @last_played_at = Date.parse(last_played_at)
   end
 
   def can_be_archived?
-    Time.now.year - Date.parse(@publish_date).year > 10 && Time.now.year - Date.parse(@last_played_at).year > 2
+    super && ((Date.today.year - @last_played_at.year) > 2)
   end
 end
