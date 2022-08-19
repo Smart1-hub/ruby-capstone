547482093f55d2a6a4c7495d7e7c441b3ea26ebd
Felix Odete <felix.odette@gmail.com>
2022-08-19 09:05:57 +0300

    Fix: authors list empty

--- data/author_data.rb
+++ data/author_data.rb
@@ -2,28 +2,28 @@ require 'json'
 
 module AuthorsController
   def add_author(author)
-    File.new('./author/authors.json', 'w+') unless File.exist?('./author/authors.json')
+    File.new('./data/authors.json', 'w+') unless File.exist?('./data/authors.json')
 
-    if File.empty?('./author/authors.json')
+    if File.empty?('./data/authors.json')
       authors = []
     else
-      data = File.read('./author/authors.json').split
+      data = File.read('./data/authors.json').split
       authors = JSON.parse(data.join)
     end
 
     authors.push({ id: author.id, first_name: author.first_name, last_name: author.last_name })
 
-    File.write('./author/authors.json', authors.to_json)
+    File.write('./data/authors.json', authors.to_json)
   end
 
   def list_authors
     puts '-' * 50
-    File.new('./data/authors.json', 'w+') unless File.exist?('./author/authors.json')
+    File.new('./data/authors.json', 'w+') unless File.exist?('./data/authors.json')
 
-    if File.empty?('./author/authors.json')
+    if File.empty?('./data/authors.json')
       puts 'The authors list is empty'
     else
-      data = File.read('./author/authors.json').split
+      data = File.read('./data/authors.json').split
       authors = JSON.parse(data.join)
       puts '🤵 Authors list:'
       authors.each_with_index do |author, index|
